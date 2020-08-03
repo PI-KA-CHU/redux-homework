4a6cea3c7175b0d1ad326904708bd09a7bee65ea
benjamin <529709737@qq.com>
2020-08-03 16:57:42 +0800

    [my-app][Benjamin] feature: add clear function when change size

--- src/component/Counter/index.jsx
+++ src/component/Counter/index.jsx
@@ -1,32 +1,45 @@
 import React from 'react'
 
-class Counter extends React.Component{
+class Counter extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {value : 0};
+        this.state = { value: 0 , groupSize : 0};
     }
 
     render() {
         return (
             <div>
-                <button onClick = {this.decrescence}>-</button>
+                <button onClick={this.decrescence}>-</button>
                 <label >{this.state.value}</label>
-                <button onClick = {this.increament}>+</button>
+                <button onClick={this.increament}>+</button>
             </div>
         )
     }
 
     increament = () => {
         this.setState((preState) => ({
-            value : preState.value + 1
+            value: preState.value + 1
         }))
+        this.props.onIncrease();
     }
 
     decrescence = () => {
         this.setState((preState) => ({
-           value : preState.value - 1 
+            value: preState.value - 1
         }))
+        this.props.onDecreate();
+    }
+
+    static getDerivedStateFromProps(props, state){
+
+        if (props.groupSize !== state.groupSize) {
+            return {
+                value: 0,
+                groupSize: props.groupSize
+            }
+        }
+        
     }
 }
 
--- src/component/CounterGroup/index.jsx
+++ src/component/CounterGroup/index.jsx
@@ -4,23 +4,42 @@ import Counter from "../Counter";
 class CounterGroup extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { size: 0 }
+        this.state = { size: 0, totalNumebr: 0 }
     }
 
     handleResize = (event) => {
         this.setState({
-            size: event.target.value ? parseInt(event.target.value) : 0
+            size: event.target.value ? parseInt(event.target.value) : 1,
+            totalNumebr: 0
         })
+    }
+
+    handelIncrease = () => {
+        this.setState((preState) => ({
+            totalNumebr: preState.totalNumebr + 1
+        }))
+    }
+
+    handelDecreate = () => {
+        this.setState((preState) => ({
+            totalNumebr: preState.totalNumebr - 1
+        }))
     }
 
     render() {
         const initArray = [...Array(this.state.size).keys()]
         return (
             <div>
-                <label>Group size: </label>
-                <input onBlur={this.handleResize} defaultValue={0} />
                 <div>
-                    {initArray.map(key => <Counter key={key} />)}
+                    <label>Group size:
+                        <input onBlur={this.handleResize} defaultValue={0} />
+                    </label>
+                </div>
+                <div>
+                    <label>Total number : {this.state.totalNumebr}</label>
+                </div>
+                <div>
+                    {initArray.map(key => <Counter onIncrease={this.handelIncrease} onDecreate={this.handelDecreate} groupSize={this.state.size} key={key} />)}
                 </div>
             </div>
         )
